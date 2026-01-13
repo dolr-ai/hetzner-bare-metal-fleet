1f941f6b957160287b1df7aa2acb6af8997ba701
Saikat Das <saikatdas0790@gmail.com>
2026-01-13 14:18:33 +0400

    Add Ubuntu 24.04 image path to installimage command

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ wipefs -fa /dev/sd*
 # for NVMe drives:
 wipefs -fa /dev/nvme*n1
 
-installimage -a -n $MACHINE_HOSTNAME -r no -l 0 -p /boot:ext3:1024M,btrfs.1:btrfs:all,btrfs.1:@:/ -d $DRIVE_TO_USE -f yes -t yes
+installimage -a -n $MACHINE_HOSTNAME -r no -l 0 -p /boot:ext3:1024M,btrfs.1:btrfs:all,btrfs.1:@:/ -d $DRIVE_TO_USE -f yes -t yes -i /root/images/Ubuntu-2404-noble-amd64-base.tar.gz
