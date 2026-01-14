3cf982cbb5bddb14b115bc4f4419f20b29a2e4b1
Saikat Das <saikatdas0790@gmail.com>
2026-01-14 06:02:44 -0800

    Update installimage command parameters for partitioning scheme

--- init.sh
+++ init.sh
@@ -95,7 +95,7 @@ echo "=========================================="
     -n "$MACHINE_HOSTNAME" \
     -r no \
     -l 0 \
-    -p /boot:ext3:1024M,/:btrfs:all \
+    -p /:btrfs:all \
     -d "$DRIVE_TO_USE" \
     -f yes \
     -t yes \
