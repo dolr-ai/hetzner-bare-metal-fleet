0139a7a43ddf8c8880d4cd756c6b2772e16068b2
Saikat Das <saikatdas0790@gmail.com>
2026-01-14 05:52:46 -0800

    Update installimage command parameters for filesystem configuration

--- init.sh
+++ init.sh
@@ -95,7 +95,7 @@ echo "=========================================="
     -n "$MACHINE_HOSTNAME" \
     -r no \
     -l 0 \
-    -p /boot:ext3:1024M,btrfs.1:btrfs:all,btrfs.1:@:/ \
+    -p /boot:ext3:1024M,/:btrfs:all \
     -d "$DRIVE_TO_USE" \
     -f yes \
     -t yes \
